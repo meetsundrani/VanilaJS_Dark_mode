4a49173d04c102bb8b9a28acc8e8a02c16e75e3e
meetsundrani <41486000+meetsundrani@users.noreply.github.com>
2022-09-10 19:18:03 +0530

    pushed all code for dark mode and light mode using local storage

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let theme = localStorage.getItem('data-theme');
+const changeThemeToDark = () => {
+    document.documentElement.setAttribute("data-theme", "dark")
+    localStorage.setItem("data-theme", "dark")
+}
+
+const changeThemeToLight = () => {
+    document.documentElement.setAttribute("data-theme", "light")
+    localStorage.setItem("data-theme", 'light')
+}
+
+if(theme === 'dark'){
+    changeThemeToDark()
+}
+
+const checkbox = document.getElementById("switch");
+checkbox.addEventListener('change', () => {
+    let theme = localStorage.getItem('data-theme');
+    if (theme ==='dark'){
+        changeThemeToLight()
+    }else{
+        changeThemeToDark()
+    }   
+});
